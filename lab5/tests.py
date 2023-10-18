a3fd31b8f895d5c724818725bf940aad57c9c635
Daniil Tereshchenko <tereshchenkodwork@gmail.com>
2023-10-18 15:34:12 +0300

    lab5 v1.0

---
+++ lab5/tests.py
@@ -0,0 +1,38 @@
+# 1
+list_1 = ["A", "B", "C"]
+list_2 = list_1
+list_3 = list_2
+
+del list_1[0]
+del list_2[0]
+
+print(list_3)
+
+# 2
+list_1 = ["A", "B", "C"]
+list_2 = list_1
+list_3 = list_2
+
+del list_1[0]
+
+print(list_3)
+
+# 3
+list_1 = ["A", "B", "C"]
+list_2 = list_1
+list_3 = list_2
+
+del list_1[0]
+del list_2[:]
+
+print(list_3)
+
+# 4
+list_1 = ["A", "B", "C"]
+list_2 = list_1[:]
+list_3 = list_2[:]
+
+del list_1[0]
+del list_2[0]
+
+print(list_3)
